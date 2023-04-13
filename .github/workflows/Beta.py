ea46bc7da0e063650a4d951807fcfe939b40e792
Battleground12 <99104224+Battleground12@users.noreply.github.com>
2023-04-13 22:14:56 +0300

    Create Beta.py

---
+++ .github/workflows/Beta.py
@@ -0,0 +1,6 @@
+curl -O https://beta.rclone.org/rclone-beta-latest-linux-arm.zip
+  unzip rclone-beta-latest-linux-arm.zip
+  cd rclone-v1.36-235-g51866fbdβ-linux-arm/
+sudo cp rclone /usr/bin/
+sudo chown root:root /usr/bin/rclone
+sudo chmod 755 /usr/bin/rclone
